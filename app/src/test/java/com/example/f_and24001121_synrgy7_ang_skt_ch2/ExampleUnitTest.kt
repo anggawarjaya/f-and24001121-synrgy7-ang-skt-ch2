d8a7b930db0e8961a9b4724ab65fede01bc52630
anggawarjaya <awfproduction.ing@gmail.com>
2024-03-29 11:38:25 +0700

    initialize project

---
+++ app/src/test/java/com/example/f_and24001121_synrgy7_ang_skt_ch2/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.f_and24001121_synrgy7_ang_skt_ch2
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
